56a207e23ee87b3dd73df10c8a56a3639bd57488
Devashish Upadhyay <devashish5113@gmail.com>
2025-03-19 00:12:54 +0530

    Initial Commit

---
+++ Search&Browse/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Search&Browse
+//
+//  Created by Devashish Upadhyay on 19/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Search&Browse/Search_BrowseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Search_BrowseApp.swift
+//  Search&Browse
+//
+//  Created by Devashish Upadhyay on 19/03/25.
+//
+
+import SwiftUI
+
+@main
+struct Search_BrowseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
